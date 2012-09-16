33bb8d1355516d6eeaae34fb5a216fed106293bb
Iván Loire <ivan@iloire.com>
2012-09-16 23:28:34 +0200

    fix resizing images IE

--- js/stufffffff.js
+++ js/stufffffff.js
@@ -1,4 +1,6 @@
-var content = {
+var app = {};
+
+app.content = {
   'map' : '<div><img src="images/zaragoza_spain.png"></div>',
   'express' : '<div><img src="images/express_js.png"></div>',
   'math_race' : '<div><img src="images/math_race02.png"></div>',
@@ -14,32 +16,39 @@ var content = {
   'real_time_apps_slides':'<div><img src="images/real_time_apps_slides01.png"></div>'
 };
 
-function preload(arrayOfImages) {
+app.preload = function (arrayOfImages) {
     $(arrayOfImages).each(function(){
         $('<img/>')[0].src = this;
     });
-}
+};
+
+app.settings = {
+  min_window_width : 767, //extra content will be shown for bigger sizes
+  left_margin_extra_content : 600,
+  offset_top : 20
+};
 
 $(document).ready(function() {
 
-  var min_window_width = 767; //extra content will be shown for bigger sizes
-  var left_margin_extra_content = 600;
-  var timeout_fade = null;
-  var offset_top = 20;
-
-  var element_extra_content = $('#extrainfo'); //cache element
+  var extra_content = $('#extrainfo'); //cache element
 
   function set_extra_content(element){
     var windowWidth = $(window).width();
-    if (element && (windowWidth > min_window_width)){
+    if (element && (windowWidth > app.settings.min_window_width)){
       var id = $(element).attr('data-contentid');
-      var top = $(window).scrollTop() + offset_top;
-      $(element_extra_content).css({top: top, left: left_margin_extra_content,
-          position: 'absolute' }).html(content[id]).fadeIn();
+      var top = $(window).scrollTop() + app.settings.offset_top;
+
+      var max_width_images = windowWidth - app.settings.left_margin_extra_content - 40;
+
+      $(extra_content).css({top: top, left: app.settings.left_margin_extra_content,
+          position: 'absolute' }).html(app.content[id]).fadeIn();
+
+      $('img', extra_content).css({'max-width': max_width_images + 'px'});
+
       $(element).addClass('active');
     }
     else{
-      $(element_extra_content).hide();
+      $(extra_content).hide();
     }
   }
 
@@ -48,13 +57,13 @@ $(document).ready(function() {
   });
 
   $('a.extra_content').mouseover(function(ev){
-    clearTimeout(timeout_fade);
+    clearTimeout(app.timeout_fade);
     set_extra_content(this);
   });
 
   $('a.extra_content').mouseout(function(ev){
     $(this).removeClass('active');
-    timeout_fade = setTimeout(function(){
+    app.timeout_fade = setTimeout(function(){
       $(element_extra_content).fadeOut('800');
     }, 1000);
   });
@@ -70,9 +79,9 @@ $(document).ready(function() {
 
   github_service.getGitHubProjects('iloire', '#ghcontainer');
 
-  if ($(window).width()>min_window_width){
+  if ($(window).width()>app.settings.min_window_width){
     //preload if we show extra content
-    preload([
+    app.preload([
         'images/zaragoza_spain.png',
         'images/express_js.png',
         'images/math_race01.png',
