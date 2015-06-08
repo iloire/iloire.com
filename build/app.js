c817ff82ef5a4e3a057c07438b7cd0dd8015f27e
Ivan Loire <iloire@atlassian.com>
2015-06-08 13:20:14 +1000

    Github previewer styling tweaks

--- build/app.js
+++ build/app.js
@@ -12288,7 +12288,7 @@ define('markdown-fetcher', ['jquery', 'service-markdown-fetcher', 'content-hover
         service.fetch('iloire', $(this).data('repo'), {cache_duration_minutes: 60}, function (data) {
           var container = $('<div/>').addClass('gh-preview');
           container.append('<span class="header">github\'s readme preview</span>');
-          container.append('<span class="help">this is just a partial preview. click on the link to go to the project repo</span>');
+          container.append('<span class="help">this is just a partial preview fetched from gh. click on the link to go to the project repo</span>');
           var html = markdown.toHTML(window.atob(data.content));
           container.append($('<div />').addClass('gh-readme').html(html));
           contentHover.showExtraContent(container);
