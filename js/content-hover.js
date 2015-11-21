b90c378f853c2e8aac3fa240600aca6dba79204a
Ivan Loire <iloire@atlassian.com>
2015-11-21 13:56:04 +1100

    feat: center text in hover content

--- js/content-hover.js
+++ js/content-hover.js
@@ -15,15 +15,15 @@ define('content-hover', [
       });
 
   var CONTENT = {
-    'dgallery': '<div><img class="full dropshadow" src="' + SETTINGS.CND_IMG_PREFIX + 'dgallery1.png"><span class="description">dGallery, a sexy asp.net mvc photo gallery</span></div>',
-    'atlassian': '<div><img class="full dropshadow" src="' + SETTINGS.CND_IMG_PREFIX + 'shipit24.jpg"></div>',
+    'dgallery': '<div class="centered"><img class="full dropshadow" src="' + SETTINGS.CND_IMG_PREFIX + 'dgallery1.png"><span class="description">dGallery, a sexy asp.net mvc photo gallery</span></div>',
+    'atlassian': '<div class="centered"><img class="full dropshadow" src="' + SETTINGS.CND_IMG_PREFIX + 'shipit24.jpg"></div>',
     'map': '<div class="map-europe"><div class=pulse></div><div class="img-wrapper"><img class="full" src="' + SETTINGS.CND_IMG_PREFIX + 'europe.svg"></div></div>',
-    '2earth': '<div><img class="full" src="' + SETTINGS.CND_IMG_PREFIX + '2earth_01.png"></div>',
-    'watchmen': '<div><img class="full dropshadow" src="' + SETTINGS.CND_IMG_PREFIX + 'watchmen.png"><span class="description">A node.js service monitor. Source code available on GitHub</span></div>',
-    'directorio_cachirulo': '<div><img class="full dropshadow" src="' + SETTINGS.CND_IMG_PREFIX + 'directorio.jpg"><span class="description">Local freelance directory. Software created with node.js and redis. Available on GitHub</span></div>',
-    'letsnode': '<div><img class="full dropshadow" src="' + SETTINGS.CND_IMG_PREFIX + 'letsnode.jpg"></div>',
-    'backbone_google_maps': '<div><img class="full dropshadow" src="' + SETTINGS.CND_IMG_PREFIX + 'backbone-googlemaps.jpg"><span class="description">Playing with Backbone.js and Google Maps..</span></div>',
-    'atlasboard': '<div><img class="full dropshadow" src="' + SETTINGS.CND_IMG_PREFIX + 'atlasboard-02.jpg"><span class="description">Atlasboard, a simple dashboard tool</span></div>'
+    '2earth': '<div class="centered"><img class="full" src="' + SETTINGS.CND_IMG_PREFIX + '2earth_01.png"></div>',
+    'watchmen': '<div class="centered"><img class="full dropshadow" src="' + SETTINGS.CND_IMG_PREFIX + 'watchmen.png"><span class="description">A node.js service monitor</span></div>',
+    'directorio_cachirulo': '<div class="centered"><img class="full dropshadow" src="' + SETTINGS.CND_IMG_PREFIX + 'directorio.jpg"><span class="description">Local freelance directory app created with node.js and redis</span></div>',
+    'letsnode': '<div class="centered"><img class="full dropshadow" src="' + SETTINGS.CND_IMG_PREFIX + 'letsnode.jpg"></div>',
+    'backbone_google_maps': '<div class="centered"><img class="full dropshadow" src="' + SETTINGS.CND_IMG_PREFIX + 'backbone-googlemaps.jpg"><span class="description">Playing with Backbone.js and Google Maps</span></div>',
+    'atlasboard': '<div class="centered"><img class="full dropshadow" src="' + SETTINGS.CND_IMG_PREFIX + 'atlasboard-02.jpg"><span class="description">Atlasboard, a simple dashboard tool</span></div>'
   };
 
   var extraContent = $('#extrainfo');
