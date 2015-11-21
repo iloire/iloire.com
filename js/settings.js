77cbd6decbbc0c2b8ac96a140535c1d50f8d8d35
Ivan Loire <iloire@atlassian.com>
2015-11-21 14:10:37 +1100

    fix: use relative path for images (the whole site is already in CDN)

--- js/settings.js
+++ js/settings.js
@@ -1,11 +1,9 @@
 define('settings', function(){
 
-  var DEV = true;
-
   var SETTINGS = {
     max_gh_projects: 7,
     min_window_width: 900, //extra content will be shown for bigger sizes
-    CND_IMG_PREFIX: DEV ? 'images/' : 'http://d13ry56xmap4ax.cloudfront.net/'
+    CND_IMG_PREFIX: 'images/'
   };
 
   return SETTINGS;
